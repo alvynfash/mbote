ee861b9f66c046201d40124a1ac685c810640ed0
Alvyn Fasuyi <zetagik@gmail.com>
2021-05-12 06:05:10 +0200

    initial startScreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 void main() {
   runApp(MboteApp());
 }
 
-class MboteApp extends StatelessWidget {
+class MboteApp extends GetMaterialApp {
+  MboteApp()
+      : super(
+          initialRoute: '/appStarter',
+          getPages: [GetPage(name: '/appStarter', page: () => AppStarter())],
+        );
+}
+
+class AppStarter extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -13,24 +22,24 @@ class MboteApp extends StatelessWidget {
           children: [
             _buildBackground(),
             Container(
-              padding: const EdgeInsets.all(32),
+              padding: const EdgeInsets.all(24),
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.end,
                 children: [
-                  _buildOnboardingText(),
-                  SizedBox(height: 80),
+                  _buildTagline(),
+                  SizedBox(height: 120),
                   _buildButtons(),
                 ],
               ),
               decoration: BoxDecoration(
                 gradient: LinearGradient(
                   colors: [
-                    Colors.white.withOpacity(.1),
-                    Colors.black.withOpacity(.65),
+                    Colors.transparent,
+                    Colors.black.withOpacity(.95),
                   ],
                   begin: Alignment.topRight,
                   end: Alignment.bottomLeft,
-                  stops: [0, .6],
+                  stops: [0, .8],
                 ),
               ),
             ),
@@ -42,32 +51,37 @@ class MboteApp extends StatelessWidget {
 
   Widget _buildBackground() {
     return Image.network(
-      'https://lh3.googleusercontent.com/proxy/Fp3MuGUpshzP_twAShQfH7xj9vdMKDP5ZJuzS6qwjmd5H6GYxY4VdN8ouGs4slNKdtuj1N5cjAzTQFihbOsi3ORIXfEEzp9xlkUGe2GLl13qnBg3n3TQwVltUDczYNjC82T4OQWIV9PQfShlA4YV',
-      fit: BoxFit.cover,
-      height: 950,
+      'https://cdn.asiatatler.com/asiatatler/i/ph/2020/07/03144009-gettyimages-1208790371_cover_2000x1334.jpg',
+      fit: BoxFit.fitHeight,
+      height: 930,
+    );
+  }
+
+  Widget _buildTagline() {
+    return Column(
+      children: [
+        Container(
+          height: 120,
+          width: 120,
+          color: Colors.grey,
+          child: Placeholder(),
+        ),
+        SizedBox(height: 24),
+        _buildOnboardingText(),
+      ],
     );
   }
 
   Widget _buildOnboardingText() {
     return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        SizedBox(
-          width: 250,
-          child: Text(
-            'Provides convenience',
-            style: TextStyle(
-                color: Colors.white, fontSize: 30, fontWeight: FontWeight.bold),
-          ),
-        ),
-        Padding(
-          padding: const EdgeInsets.only(top: 12),
-          child: Text(
-            'Eliminates the hastle of going to the market to purchase groceries',
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 18,
-            ),
+        Text(
+          'Acess simple aux meilleur produits pas cher',
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 36,
+            fontWeight: FontWeight.bold,
           ),
         ),
       ],
@@ -79,7 +93,14 @@ class MboteApp extends StatelessWidget {
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
         TextButton(
-          onPressed: () => print('Get started tapped'),
+          onPressed: () => Get.bottomSheet(
+            Container(
+              // color: Colors.white,
+              height: 300,
+            ),
+            backgroundColor: Colors.white,
+            barrierColor: Color(0xffF1B256),
+          ),
           child: Text('Get Started'),
           style: TextButton.styleFrom(
             primary: Colors.black,
@@ -92,7 +113,7 @@ class MboteApp extends StatelessWidget {
         Center(
           child: InkWell(
             child: Padding(
-              padding: const EdgeInsets.symmetric(vertical: 16),
+              padding: const EdgeInsets.symmetric(vertical: 24),
               child: Text(
                 'Log in',
                 style: TextStyle(
